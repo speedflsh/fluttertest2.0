7ad9b956710726164b3c232f922262c3910f1f9b
shadow <anuragverma3790@gmail.com>
2025-04-22 17:53:52 +0530

    start

---
+++ lib/Second.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+class currency extends StatelessWidget{
+  const currency({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text("hello world"),
+      ),
+    );
+  }
+
+}

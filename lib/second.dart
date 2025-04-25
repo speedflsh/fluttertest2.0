73a35c577bb7a101d3f5c3b1d722fc43ec960d8f
shadow <anuragverma3790@gmail.com>
2025-04-25 16:00:47 +0530

    constproblem

--- lib/second.dart
+++ lib/second.dart
@@ -4,22 +4,22 @@ class Currency extends StatelessWidget{
   const Currency({super.key});
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
+    return  Scaffold(
       backgroundColor: Color.fromRGBO(0, 0, 0, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [ Text('0',
+          children: [ const Text('0',
           style: TextStyle(
             fontSize:100,
             fontWeight: FontWeight.w900,
             color:Color.fromARGB(216, 255, 0, 170),
           ),),
           TextField(
-            style:TextStyle(color :Colors.orange),
+            style:const TextStyle(color :Colors.orange),
             decoration: InputDecoration(
               label: Text("pleaase enter in no ",
-              style: TextStyle(color:Colors.black),
+              style:const  TextStyle(color:Colors.black),
               ),
               prefixIcon: Icon(Icons.monetization_on_outlined),
               prefixIconColor: Colors.black,
@@ -29,9 +29,12 @@ class Currency extends StatelessWidget{
                 borderSide: const BorderSide(
                   color: Colors.red,
                   width :2.0,
-                  style: BorderStyle.solid
+                  style: BorderStyle.solid,
                 ),
+                borderRadius:   BorderRadius.circular(60),
+
               ),
+
             ),
           ),
           ],),)
